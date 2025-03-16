4266f80da0863d0ecd51bb3b4ff1ec9cb5400044
GMROCKERZ <gpl31.msh@gmail.com>
2025-03-16 23:16:06 +0530

    DS ALgo day 2/300

---
+++ ArraysAndString/src/TwoSum/TargetSum.java
@@ -0,0 +1,35 @@
+package TwoSum;
+
+public class TargetSum
+{
+    public static boolean checkForTarget(int[] nums, int target)
+    {
+        int left =0;
+        int right = nums.length-1;
+
+        while(left < right)
+        {
+            int curr= nums[left]+nums[right];
+            if(curr == target)
+            {
+                return true;
+            }
+            if(curr>target)
+            {
+                right--;
+            }else
+            {
+                left++;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args)
+    {
+        int[] nums = {1,2,4,6,8,9,14,15};
+        int target = 13;
+
+        boolean result = checkForTarget(nums,target);
+        System.out.printf("given array contains the target sum: "+result);
+    }
+}
