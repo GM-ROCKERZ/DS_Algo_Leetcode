7f61428fed2dccba1a800bc716304285a2d3423c
GMROCKERZ <gpl31.msh@gmail.com>
2025-05-24 21:33:23 +0530

    DS ALgo day 3/300

---
+++ ArraysAndString/src/TwoSum/CombiningSortedArray.java
@@ -0,0 +1,46 @@
+package TwoSum;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CombiningSortedArray
+{
+    public static List<Integer>  combine(int[] arr1,int[] arr2){
+        List<Integer> ans = new ArrayList<Integer>();
+        int i =0;
+        int j=0;
+
+        while(i<arr1.length && j<arr2.length)
+        {
+            if(arr1[i] < arr2[j]) {
+                ans.add(arr1[i]);
+                i++;
+            }
+            else
+            {
+                ans.add(arr2[j]);
+                j++;
+
+            }
+        }
+        while(i<arr1.length)
+        {
+            ans.add(arr1[i]);
+            i++;
+        }
+        while(j<arr2.length)
+        {
+            ans.add(arr2[j]);
+            j++;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args)
+    {
+        int[] arr1 = {1,4,7,20};
+        int[] arr2 = {3,5,6};
+        List<Integer> result = combine(arr1,arr2);
+        System.out.println(result);
+    }
+}
---
+++ ArraysAndString/src/TwoSum/StringSubsequence.java
@@ -0,0 +1,27 @@
+package TwoSum;
+
+public class StringSubsequence {
+
+    public static  boolean isSubsequence(String s,String t)
+    {
+        int i =0;
+        int j=0;
+
+        while(i<s.length() && j<t.length())
+        {
+            if(s.charAt(i) == t.charAt(j))
+            {
+                i++;
+            }
+            j++;
+        }
+        return i == s.length();
+    }
+
+    public static void main(String[] args)
+    {
+        String s = "ace";
+        String t = "abcde";
+        System.out.println("Given string s is subsequence of t: "+isSubsequence(s,t));
+    }
+}
